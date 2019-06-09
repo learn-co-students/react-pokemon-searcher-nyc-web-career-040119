be1bf0b0c028a621d3b6f78df05ad0a2fb0531c9
Jeffrey Marks <JKDMarks@gmail.com>
2019-06-09 18:23:12 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    showingFront: true
+  }
+
+  handleClick = () => this.setState({showingFront: !this.state.showingFront})
+
   render() {
+    // {height, weight, id, name, abilities, moves, stats, types, sprites}
+    const {id, name, stats, sprites} = this.props.pokemon
+    const hpStat = stats.find(stat => stat.name === "hp")
+
     return (
       <Card>
-        <div>
+        <div onClick={this.handleClick}>
           <div className="image">
-            <img alt="oh no!" />
+            {
+              this.state.showingFront ?
+              <img src={sprites.front} alt={name + " front"} /> :
+              <img src={sprites.back} alt={name + " back"} />
+            }
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hpStat.value}hp
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,54 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemons: [],
+    currSearch: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(r => r.json())
+      .then(pokemons => this.setState({pokemons, currSearch: pokemons}))
+  }
+
+  pokemonSearch = e => {
+    // I COULDN'T FIND A WAY TO GET e.persist() TO WORK WITH DEBOUNCE
+    const pokeInput = document.querySelector("#pokemon-search-input").value
+    if (pokeInput === "") {
+      this.setState({currSearch: this.state.pokemons})
+    } else if (pokeInput.substr(pokeInput.length - 2, 2) === "hp") {
+      const filterPokes = this.state.pokemons.filter(poke => {
+        const pokeHp = poke.stats.find(stat => stat.name === "hp")
+
+        return parseInt(pokeInput.slice(0, pokeInput.length-2)) === pokeHp.value
+      })
+
+      this.setState({currSearch: filterPokes})
+    } else {
+      const pokeRegex = new RegExp(pokeInput, 'gi')
+      const filterPokes = this.state.pokemons.filter(poke => {
+        return poke.name.match(pokeRegex)
+      })
+
+      this.setState({currSearch: filterPokes})
+    }
+  }
+
+  addToPokemons = pokemon => this.setState({
+    pokemons: [...this.state.pokemons, pokemon]
+  })
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search id="pokemon-search-input" onSearchChange={_.debounce(this.pokemonSearch, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.currSearch}/>
         <br />
-        <PokemonForm />
+        <PokemonForm addToPokemons={this.addToPokemons}/>
       </div>
     )
   }
