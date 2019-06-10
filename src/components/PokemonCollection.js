d226e670d30456883bd3d128c1c85e12b63914c8
ferryrules <ferris@ferryrules.com>
2019-06-10 12:02:25 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,18 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+  eachPokemon = () => {
+    return this.props.pokemon.map(poke=>{
+      return <PokemonCard poke={poke} key={poke.name} />
+    })
+  }
+
   render() {
+    console.log("collection", this.props);
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.eachPokemon()}
       </Card.Group>
     )
   }
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -3,18 +3,55 @@ import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
+const API = 'http://localhost:3000/pokemon'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    searchTerm: ''
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(r=>r.json())
+    .then(pokemon=>{
+      this.setState({
+        pokemon
+      })
+    })
+  }
+
+  searching = (e, { value }) => {
+    // console.log(e.currentTarget.value);
+    this.setState({
+      searchTerm: value
+    })
+  }
+
+  addPokemon = (poke) => {
+    this.setState({
+      pokemon: [...this.state.pokemon, poke]
+    })
+  }
+
   render() {
+    // console.log("index", this.state.pokemons);
+    const { pokemon } = this.state
+    const filteredPoke =  this.state.pokemon.filter(p => {
+      const set = this.state.searchTerm.toLowerCase()
+      return (p.types.join(" ").includes(set) || p.name.includes(set))
+    })
+    // console.log(filteredPoke);
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.searching,500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonForm addPokemon={this.addPokemon}/>
         <br />
-        <PokemonForm />
+        <PokemonCollection pokemon={filteredPoke.length > 0 ? filteredPoke : pokemon}/>
       </div>
     )
   }
